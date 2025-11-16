fd9ea7535a9e40d43a2a53dd15fd10491b1a4378
mercey123 <erik1telpt2@gmail.com>
2025-11-16 10:34:06 +0300

    feat: add mini game

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,17 @@
         <NuxtLink to="/" class="font-bold text-xl"> EpiGen </NuxtLink>
       </template>
       <template #right>
+        <UModal fullscreen>
+          <template #body>
+            <iframe
+              src="https://chrome-try-53324901.figma.site/"
+              class="w-full h-full border-0 rounded-lg"
+              title="Mini game about your financial life"
+            />
+          </template>
+
+          <UButton variant="soft" label="Mini game about your financial life" />
+        </UModal>
         <SkillSettingsPanel />
       </template>
     </UHeader>
